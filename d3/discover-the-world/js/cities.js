5e791090574f21471c12beb9ec51df8b9b9a6ca6
Fausto Pérez <faustoperez@gmail.com>
2019-10-23 10:21:08 +0200

    discover the world progress

--- d3/discover-the-world/js/cities.js
+++ d3/discover-the-world/js/cities.js
@@ -13,12 +13,79 @@ svg
     .attr("width", 960)
     .attr ("height", 720)
 
+const axisXGroup = svg
+    .append("g")
+    .attr("class", "x-axis")
+    .attr("transform", "translate(0, 620)")
+
+const axisYGroup = svg
+    .append("g")
+    .attr("class", "y-axis")
+    .attr("transform", "translate(100, 0)")
+
+const axisXText = svg
+    .append("text")
+    .attr("class", "x-axis")
+    .attr("transform", "translate(480, 670)")
+    .text("x axis")
+
+const axisYText = svg
+    .append("text")
+    .attr("class", "y-axis")
+    .attr("transform", "translate(20, 360) rotate(-90)")
+    .text("y axis")
 
 
 const placeCities = function() {
    
-    let valueX = "singlePerson"
-    let valueY = "apartment"
+    let inputX = document.querySelector("select[name=valueX]")
+    let inputY = document.querySelector("select[name=valueY]")
+
+    let valueX = inputX.value
+    let valueY = inputY.value
+
+    let textX =inputX.options[inputX.selectedIndex].innerHTML
+    let textY =inputY.options[inputY.selectedIndex].innerHTML
+
+    axisXText.text(textX)
+    axisYText.text(textY)
+
+    // max value from the data
+
+    let maxValueX = d3.max(data, (d,i) => { return d[valueX]})
+    let maxValueY = d3.max(data, (d,i) => { return d[valueY]})
+    let maxValueR = d3.max(data, (d,i) => { return d.population })
+
+    const scaleX = d3.scaleLinear()
+        .domain([0, maxValueX])
+        .range([100, 860])
+    
+    const scaleY = d3.scaleLinear()
+        .domain([0, maxValueY])
+        .range([620, 100])
+    
+    const scaleR = d3.scaleSqrt()
+        .domain([0, maxValueR])
+        .range([0, 30])    
+
+
+    const axisX = d3.axisBottom(scaleX)
+        .tickSizeInner(-520)
+        .tickSizeOuter(0)
+        .tickPadding(10)
+        .ticks(10, "$,f")
+
+
+    const axisY = d3.axisLeft(scaleY)
+        .tickSizeInner(-760)
+        .tickSizeOuter(0)
+        .tickPadding(10)
+        .ticks(10, "$,f")
+
+    axisXGroup.call(axisX)
+    axisYGroup.call(axisY)
+
+
 
     const cities = svg
         .selectAll("g.city")
@@ -26,13 +93,29 @@ const placeCities = function() {
         .enter()
         .append("g")
         .attr("class", "city")
-        .attr("transform", (d, i) => { return `translate(${i * 50}, 0)`})
+        .attr("transform", (d, i) => { 
+            const x = scaleX(d[valueX])
+            const y = scaleY(d[valueY])
+            return `translate(${x}, ${y})`
+        })
     
     cities
         .append("circle")
-        .attr("cx", 40)
-        .attr("cy", 40)
-        .attr("r", 40)
+        .attr("cx", 0)
+        .attr("cy", 0)
+        .attr("r", 0)
+        .transition()
+        .attr("r", (d, i) => { return scaleR(d.population) })
+
+    svg
+        .selectAll("g.city")
+        .transition()
+        .duration(500)
+        .attr("transform", (d, i) => { 
+            const x = scaleX(d[valueX])
+            const y = scaleY(d[valueY])
+            return `translate(${x}, ${y})`
+        })
 
 }
 
