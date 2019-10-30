5a0041ca4ea24c0363d5adaf4378853f3991393a
Fausto Pérez <faustoperez@gmail.com>
2019-10-30 09:55:27 +0100

    Start Kiddo Kiddo project

---
+++ d3/kiddo-kiddo/js/kiddo.js
@@ -0,0 +1,28 @@
+
+const formTag = document.querySelector("form")
+const inputTag = formTag.querySelector("input")
+const nameTag = d3.select("span.name")
+
+const search = function (name) {
+    let results = data.filter((d, i) => {
+        return d.name.toLowerCase() == name.toLowerCase()
+    })
+
+    if (results.length > 0) {
+        nameTag.text(name)
+    } else {
+        alert(`No results for ${name}!`)
+    }
+
+}
+
+search("Richard")
+
+formTag.addEventListener("submit", function(event) {
+    event.preventDefault()
+
+    search(inputTag.value)
+
+    inputTag.value = ""
+
+})
