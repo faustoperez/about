9b552fbc7a5310a7617cac83096edf6179c850ed
Fausto Pérez <faustoperez@gmail.com>
2019-11-11 15:32:29 +0100

    correct lost files

--- d3/kiddo-kiddo/js/kiddo.js
+++ d3/kiddo-kiddo/js/kiddo.js
@@ -3,6 +3,33 @@ const formTag = document.querySelector("form")
 const inputTag = formTag.querySelector("input")
 const nameTag = d3.select("span.name")
 
+const svg = d3.select("svg")
+
+svg
+    .attr("width", 960)
+    .attr("height", 540)
+
+const PathsGroup = svg
+    .append("g")
+    .attr("class", "paths")
+
+const rankScale = d3.scaleLinear()
+    .domain([1, 1000])
+    .range([20, 500])
+
+
+const dateScale = d3.scaleLinear()
+    .domain([1880, 2010])
+    .range([80, 915])
+
+
+const line = d3.line()
+    .x((d, i) => { return dateScale(1880 + 10 * i) })
+    .y((d, i) => { return rankScale(d) })
+    .defined((d, i) => { return d != 0 })
+    .curve(d3.curveCardinal.tension(0.5))
+
+
 const search = function (name) {
     let results = data.filter((d, i) => {
         return d.name.toLowerCase() == name.toLowerCase()
@@ -10,6 +37,17 @@ const search = function (name) {
 
     if (results.length > 0) {
         nameTag.text(name)
+
+        const lines = PathsGroup
+            .selectAll("path")
+            .data(results, (d, i) => { return d.name })
+        
+        lines
+            .enter()
+            .append("path")
+            .attr("class", (d, i) => { return d.sex })
+            .attr("d", (d, i) => { return line(d.rank) })
+
     } else {
         alert(`No results for ${name}!`)
     }
