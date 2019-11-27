8e3650fa76a947b156e68c08716299857acd6963
Fausto Pérez <faustoperez@gmail.com>
2019-11-27 11:06:04 +0100

    Finish boundless project

--- d3/kiddo-kiddo/js/kiddo.js
+++ d3/kiddo-kiddo/js/kiddo.js
@@ -13,7 +13,8 @@ const PathsGroup = svg
     .append("g")
     .attr("class", "paths")
 
-const rankScale = d3.scaleLinear()
+const rankScale = d3.scalePow()
+    .exponent(0.1)
     .domain([1, 1000])
     .range([20, 500])
 
@@ -22,6 +23,13 @@ const dateScale = d3.scaleLinear()
     .domain([1880, 2010])
     .range([80, 915])
 
+const rankAxis = d3.axisLeft(rankScale)
+    .tickValues([1, 5, 10, 25, 50, 75, 100, 500, 750, 1000])
+    .tickPadding(5)
+
+const dateAxis = d3.axisBottom(dateScale)
+    .tickFormat((d, i) => { return d + "s"})
+    .tickPadding(5)
 
 const line = d3.line()
     .x((d, i) => { return dateScale(1880 + 10 * i) })
@@ -29,6 +37,22 @@ const line = d3.line()
     .defined((d, i) => { return d != 0 })
     .curve(d3.curveCardinal.tension(0.5))
 
+const flatLine = d3.line()
+    .x((d, i) => { return dateScale(1880 + 10 * i) })
+    .y((d, i) => { return rankScale(1000) })
+    .defined((d, i) => { return d != 0 })
+    .curve(d3.curveCardinal.tension(0.5))
+
+svg
+    .append("g")
+    .attr("transform", "translate(60, 0)")
+    .call(rankAxis)
+
+svg
+    .append("g")
+    .attr("transform", "translate(0, 520)")
+    .call(dateAxis)
+    
 
 const search = function (name) {
     let results = data.filter((d, i) => {
@@ -46,7 +70,21 @@ const search = function (name) {
             .enter()
             .append("path")
             .attr("class", (d, i) => { return d.sex })
+            .attr("d", (d, i) => { return flatLine(d.rank) })
+            .style("opacity", 0)
+            .transition()
+            .duration(800)
+            .style("opacity", 1)
             .attr("d", (d, i) => { return line(d.rank) })
+        
+        lines
+            .exit()
+            .style("opacity", 1)
+            .transition()
+            .duration(800)
+            .style("opacity", 0)
+            .attr("d", (d, i) => { return flatLine(d.rank) })
+            .remove()
 
     } else {
         alert(`No results for ${name}!`)
