cf0448762bc893ad3a5967d02602ca4eb14db44d
Fausto Pérez <faustoperez@gmail.com>
2019-12-04 09:55:08 +0100

    start map project

---
+++ d3/population-densities/js/script.js
@@ -0,0 +1,31 @@
+const svg = d3.select("svg")
+
+svg.attr("viewbox", "0 0 1000 600")
+
+const worldGroup = svg.append("g")
+
+const projection = d3.geoNaturalEarth1()
+    .translate([500, 300])
+    .scale(175)
+
+
+const mapGenerator = d3.geoPath()
+    .projection(projection)
+
+d3.json("data.json").then(function (data) {
+    d3.json("world-110m2.json").then(function (mapData) {
+           
+        worldGroup
+            .selectAll("path")
+            .data(mapData.features)
+            .enter()
+            .append("path")
+            .attr("d", mapGenerator)
+            .style("fill", "red")
+
+
+
+    }) 
+})
+
+
